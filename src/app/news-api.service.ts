8368c391e1e2f2ded18d84af3241e77817d12b7b
MOHAMMAD HUSSAIN <zordanmuhammad@gmail.com>
2022-06-18 15:42:02 +0530

    updated

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+
+  api_key = 'c5420b85b8a249ada21c155fefa532e4';
+
+
+  constructor(private http: HttpClient) { }
+
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+
+
+
+}
